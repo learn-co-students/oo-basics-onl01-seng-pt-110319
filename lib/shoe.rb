77ff0ec9a909781997c039a9a939cba65694ab3f
Henry Phan <h.phan06@gmail.com>
2019-11-20 00:47:02 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,20 +1,14 @@
-require 'pry'
-class Shoe
-attr_accessor :brand, :color, :size, :material, :condition, :cobble
-
-  def initialize(shoe_brand)
-    @brand = shoe_brand
+class Shoe 
+  attr_accessor :brand, :color, :size, :material, :condition
+  
+  def initialize(brand)
+    @brand = brand
   end
-
-  def brand
-    @brand
-  end
-
+  
   def cobble
-    @cobble
-    puts "Your shoe is as good as new!"
     @condition = "new"
-  end
-
+    puts "Your shoe is as good as new!"
+end
+  
 end
 
