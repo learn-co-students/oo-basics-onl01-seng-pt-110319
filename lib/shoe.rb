43e911b536753e0f32e4bc55139e18fc12b4a925
qiong sun <joannesun44@gmail.com>
2019-11-24 03:39:18 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,27 @@
 # Make your shoe class here!
+class Shoe
+  def initialize (brand)
+    @brand = brand
+  end 
+  
+  attr_accessor :brand, :color, :size, :material, :condition
+
+def cobble.new
+   puts "Your shoe is as good as new!"
+ end 
+
+end 
+
+#   describe '#cobble' do
+#     it 'says that the shoe has been repaired' do
+#       expect($stdout).to receive(:puts).with("Your shoe is as good as new!")
+#       shoe.cobble
+#     end
+
+#     it 'makes the shoe\'s condition new' do
+#       shoe.condition = "old"
+#       shoe.cobble
+#       expect(shoe.condition).to eq("new")
+#     end
+#   end
+# end
