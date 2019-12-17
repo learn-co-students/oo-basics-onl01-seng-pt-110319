0ee35e5cf9f689d08320306620667e2d39322919
Jordan Hayes <nosafoundation@gmail.com>
2019-12-17 06:57:32 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,15 @@
-# Make your shoe class here!
+class Shoe
+  attr_accessor :brand, :color, :size, :material, :condition, :cobble
+
+  def initialize(brand)
+    @brand = brand
+  end
+
+  def brand
+    @brand
+  end
+
+  def turn._page
+    puts "Flipping the page...wow, you read fast!"
+  end.
+end
